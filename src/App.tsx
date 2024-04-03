406fdd65848fad00093cc4cfd1df271ae83a068c
mahenv <mahendrauppari044@gmail.com>
2024-04-03 14:21:45 -0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl bg-red-300 font-bold underline">Hello world!</h1>{" "}
+      <Home/>      
     </>
   );
 }
-
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h2 className='text-2xl font-bold'>Welcome to Car Service Appointment booking system !</h2>
+        <h3 className='text-xl'>Please Login/Register to book your appointment</h3>
+    </div>
+  )
+}
+
+export default Home;
